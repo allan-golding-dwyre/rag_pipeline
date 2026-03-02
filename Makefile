e884f8bf271cdea53ef33b6dd0ded98f02f95a02
allan-golding-dwyre <allan.golding-dwyre@vidal.fr>
2026-03-02 14:53:23 +0100

    # Remove Settings from .env file Maintenant, c'est la responsabilité de config.py de définir les settings du RAG (models names, chunk_size ...) Cela permet de voir directement dans le code, de partager les settings lors de fork, clone. etc

--- Makefile
+++ Makefile
@@ -15,9 +15,6 @@ install:
 
 build-image:
 	docker build . -t chainlit \
-		--build-arg EMBEDDING_MODEL=${EMBEDDING_MODEL}\
-		--build-arg SPARSE_EMBEDDING_MODEL=${SPARSE_EMBEDDING_MODEL}\
-		--build-arg LANGUAGE_MODEL=${LANGUAGE_MODEL} \
 		--build-arg MISTRAL_KEY=${MISTRAL_KEY} \
 		--build-arg QDRANT_KEY=${QDRANT_KEY} \
 		--build-arg QDRANT_URL=${QDRANT_URL} \
@@ -25,8 +22,3 @@ build-image:
 		--build-arg LANGFUSE_PUBLIC_KEY=${LANGFUSE_PUBLIC_KEY} \
 		--build-arg LANGFUSE_SECRET_KEY=${LANGFUSE_SECRET_KEY} \
 		--build-arg LANGFUSE_HOST_URL=${LANGFUSE_HOST_URL} \
-		--build-arg CHUNK_SIZE=${CHUNK_SIZE} \
-		--build-arg CHUNK_OVERLAP=${CHUNK_OVERLAP} \
-		--build-arg TOP_K=${TOP_K} \
-		--build-arg THRESHOLD=${THRESHOLD} \
-		--build-arg VERBOSE=${VERBOSE}
--- src/config.py
+++ src/config.py
@@ -5,9 +5,12 @@ import os
 load_dotenv()
 
 # ====== Models ======
-EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
-SPARSE_EMBEDDING_MODEL = os.getenv("SPARSE_EMBEDDING_MODEL")
-LANGUAGE_MODEL = os.getenv("LANGUAGE_MODEL")
+# EMBEDDING_MODEL='hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+# LANGUAGE_MODEL='hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
+EMBEDDING_MODEL='mistral-embed'
+SPARSE_EMBEDDING_MODEL='Qdrant/bm25'
+LANGUAGE_MODEL='mistral-small-2506'
+
 # --- Auth ---
 MISTRAL_KEY = SecretStr(os.getenv("MISTRAL_KEY"))
 
@@ -24,8 +27,11 @@ LANGFUSE_SECRET_KEY = os.getenv("LANGFUSE_SECRET_KEY")
 LANGFUSE_HOST_URL = os.getenv("LANGFUSE_HOST_URL")
 
 # ====== SETTINGS ======
-CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))
-CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP"))
-TOP_K = int(os.getenv("TOP_K"))
-THRESHOLD = float(os.getenv("THRESHOLD"))
-VERBOSE = True if os.getenv("VERBOSE") == "True" else False
+CHUNK_SIZE=400
+CHUNK_OVERLAP=100
+TOP_K=4
+THRESHOLD=0.0
+VERBOSE=False
+
+# --- online fetching ---
+CHAPTERS_TO_FETCH = ["About", "Manual"]
