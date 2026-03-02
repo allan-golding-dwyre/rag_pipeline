716320351b511dad2c8fe1ee67c4c3e6a112247d
allan-golding-dwyre <allan.golding-dwyre@vidal.fr>
2026-03-02 15:14:04 +0100

    # Embedding possible à partir du main uv run main.py Avec des arguments qui peuvent être passée pour faciliter le choix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#uv run chainlit run src/chainlit_app.py
+import argparse
 from pathlib import Path
 
 from src.document_indexer import DocumentIndexer
@@ -6,15 +6,52 @@ from src.file_documentation_loader import FileDocumentationLoader
 from src.online_documentation_loader import OnlineDocumentationLoader
 
 
-def embedding():
-    documents_path = "documents"
-    documents_paths = list(Path(documents_path).glob("*.html"))
+def main():
+    if args.source == "file":
+        documents_paths = list(Path(args.path).glob("*.html"))
+        loader = FileDocumentationLoader(documents_paths, verbose=args.verbose)
+    else:
+        loader = OnlineDocumentationLoader(
+            base_url=args.base_url,
+            verbose=args.verbose
+        )
 
-    loader = FileDocumentationLoader(documents_paths)
+    # --- Indexing ---
     indexer = DocumentIndexer()
-
     indexer.index_documents(loader)
 
-if __name__ == '__main__':
-    # embedding()
-    print(len(OnlineDocumentationLoader().load()))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Index Godot documentation (online or local files)"
+    )
+
+    parser.add_argument(
+        "--source",
+        choices=["online", "file"],
+        default="online",
+        help="Source of documentation (default: online)"
+    )
+
+    parser.add_argument(
+        "--path",
+        type=str,
+        default="documents",
+        help="Path to local HTML files (required if source=file)"
+    )
+
+    parser.add_argument(
+        "--base-url",
+        type=str,
+        default="https://docs.godotengine.org/en/stable/index.html",
+        help="Base URL for online documentation"
+    )
+
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        help="Enable verbose mode"
+    )
+
+    args = parser.parse_args()
+    main()
