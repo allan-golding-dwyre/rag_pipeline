1ca5572315148018b68b22e70e242ffea10fa4f6
allan-golding-dwyre <allan.golding-dwyre@vidal.fr>
2026-03-03 13:49:23 +0100

    Online with delay and less workers to avoid cloudfare ban due too "Too much Requests"

--- src/online_documentation_loader.py
+++ src/online_documentation_loader.py
@@ -1,3 +1,4 @@
+import time
 from typing import Iterable
 
 from bs4 import BeautifulSoup
@@ -11,24 +12,28 @@ from src.base_documentation_loader import BaseDocumentationLoader
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class OnlineDocumentationLoader(BaseDocumentationLoader):
-    def __init__(self, base_url = "https://docs.godotengine.org/en/stable/index.html#", verbose=False):
+    def __init__(self, base_url = "https://docs.godotengine.org/en/stable/index.html", verbose=False):
         super().__init__(verbose)
         self.base_url = base_url
         self.session = requests.Session()
+        self.headers={
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121 Safari/537.36"
+        }
 
         adapter = HTTPAdapter(pool_connections=20, pool_maxsize=20)
         self.session.mount("http://", adapter)
         self.session.mount("https://", adapter)
 
-        self.max_workers = 40
+        self.max_workers = 10
 
     def fetch(self, url: str):
-        response = self.session.get(url, timeout=10)
+        time.sleep(0.5)
+        response = self.session.get(url, timeout=10, headers=self.headers)
         response.raise_for_status()
         return response.content
 
     def _get_html_sources(self) -> Iterable[str]:
-        print("\tGetting HTML sources...")
+        print("Getting HTML sources...")
         urls = self._get_chapter_urls()
 
         with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
@@ -40,7 +45,10 @@ class OnlineDocumentationLoader(BaseDocumentationLoader):
         self.session.close()
 
     def _get_chapter_urls(self):
-        response = requests.get(self.base_url)
+        response = requests.get(self.base_url, headers=self.headers)
+        if response.status_code == 429:
+            raise Exception("Too Many Requests")
+
         soup = BeautifulSoup(response.content, "lxml")
         nav = soup.find("div", class_="wy-menu wy-menu-vertical")
         urls = set()
