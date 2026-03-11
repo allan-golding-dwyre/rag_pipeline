9993938d60a128b7006c95a7bfb14d44d4494bdb
allan-golding-dwyre <allan.golding-dwyre@vidal.fr>
2026-03-11 10:21:45 +0100

    remove documents to clean project

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ from src.documentation_loader.doc_parser import GodotDocParser
 from rich import print
 
 tests_urls= [
-    # "https://docs.godotengine.org/en/stable/tutorials/best_practices/introduction_best_practices.html",
-    #  "https://docs.godotengine.org/en/stable/about/introduction.html",
-    # "https://docs.godotengine.org/en/stable/classes/class_%40gdscript.html",
-    # "https://docs.godotengine.org/en/stable/classes/class_aescontext.html",
-    # "https://docs.godotengine.org/en/stable/tutorials/animation/animation_track_types.html",
-    # "https://docs.godotengine.org/en/stable/about/system_requirements.html#godot-editor",
-    # "https://docs.godotengine.org/en/stable/classes/class_audioserver.html",
-    # "https://docs.godotengine.org/en/stable/classes/class_animatedsprite2d.html"
+    "https://docs.godotengine.org/en/stable/tutorials/best_practices/introduction_best_practices.html",
+     "https://docs.godotengine.org/en/stable/about/introduction.html",
+    "https://docs.godotengine.org/en/stable/classes/class_%40gdscript.html",
+    "https://docs.godotengine.org/en/stable/classes/class_aescontext.html",
+    "https://docs.godotengine.org/en/stable/tutorials/animation/animation_track_types.html",
+    "https://docs.godotengine.org/en/stable/about/system_requirements.html#godot-editor",
+    "https://docs.godotengine.org/en/stable/classes/class_audioserver.html",
+    "https://docs.godotengine.org/en/stable/classes/class_animatedsprite2d.html"
     "https://docs.godotengine.org/en/stable/about/list_of_features.html"
 ]
 
